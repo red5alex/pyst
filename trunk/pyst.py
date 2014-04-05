77ef729122e2a56dc9cc3f688ae917c5c3cb1580
red5alex <red5alex@a9a4edef-5753-485f-8c01-a0eb9a0e8964>
2014-04-05 07:16:32 +0000

    added slope calculation to JacTestResults Class

--- trunk/pyst.py
+++ trunk/pyst.py
@@ -31,6 +31,7 @@ class PestCtrlFile:
 class JacTestResultsFile:
     paramValues = []
     obsValues = {}
+    slopes = {}
     def __init__(self,filename):
         self.load(filename)
 
@@ -38,12 +39,11 @@ class JacTestResultsFile:
         obsfile = open(filename)
         #import observations
         line = obsfile.readline(); #header
-        null, *self.paramValues = line.split()
+        null, *self.paramValues = line.split() #
         self.paramValues = list(map(float, self.paramValues)) #convert to float
 
 
         #process lines
-        #line = obsfile.readline()
         lines = obsfile.readlines()
         obsfile.close()
         for line in lines:
@@ -51,3 +51,21 @@ class JacTestResultsFile:
             obs_name, *obs_values = line.split()
             obs_values = list(map(float, obs_values)) #convert to float
             self.obsValues[obs_name] = obs_values
+
+    def calcSlopes(self):
+        for ob in self.obsValues:
+            ov = self.obsValues[ob]
+            self.slopes[ob] = []
+            for v in ov:
+                i = ov.index(v)
+                if i == len(ov)-1:
+                    pass #
+                else:
+                   o1 = v
+                   o2 = ov[i+1]
+                   p1 = self.paramValues[i]
+                   p2 = self.paramValues[i+1]
+                   slope = (o2 - o1) / (p2 - p1)
+                   self.slopes[ob].append(slope) # append the slope to the list
+
+
