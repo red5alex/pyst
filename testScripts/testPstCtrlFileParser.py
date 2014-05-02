c1d123cb6d07e58d56f85ec0ad715df4547278f5
Alexander Renz <alex.renz@gmx.net>
2014-05-02 17:56:32 +0000

    added some testScripts

---
+++ testScripts/testPstCtrlFileParser.py
@@ -0,0 +1,16 @@
+__author__ = 'are'
+
+import pystpak
+
+example_pstfile = "example_files//dam_jactest.pst"
+
+#testPstFile = pyst.PestCtrlFile("example_files//dam_jactest.pst")
+
+#testPstFile.loadBlocks("example_files//dam_jactest.pst")
+
+#testBfile = pyst.BlockFile(example_pstfile)
+
+testPstFile = pystpak.PestCtrlFile(example_pstfile)
+
+
+pass
