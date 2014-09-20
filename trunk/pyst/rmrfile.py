1df7cf0504c7c9b8a802e54a121a395b04a6d920
red5alex <red5alex@a9a4edef-5753-485f-8c01-a0eb9a0e8964>
2014-09-20 07:45:25 +0000

    - recognizes additional events

--- trunk/pyst/rmrfile.py
+++ trunk/pyst/rmrfile.py
@@ -2,27 +2,29 @@ __author__ = 'are'
 
 import datetime
 
+
 class RunManagementRecord:
     case = ""
     protocol = ""
     parlam = -1
-    slowRunFactor = -1.
+    slowrunfactor = -1.
     events = []
+    nodes = {}
 
     class Events:
 
         class GeneralEvent:
-            def __init__(self, messagetext, type="unknown"):
+            def __init__(self, messagetext, eventtype="unknown"):
                 self.message = messagetext
-                self.type = type
+                self.type = eventtype
 
         class AssignmentEvent:
-            def __init__(self, messagetext, timestamp, nodeindex, workingdir):
+            def __init__(self, messagetext, timestamp, nodeindex, workdir):
                 self.message = messagetext
                 self.type = "Assignment"
                 self.timestamp = timestamp
                 self.node = int(nodeindex)
-                self.workdir = workingdir
+                self.workdir = workdir
 
         class RunCommencementEvent:
             def __init__(self, messagetext, timestamp, nodeindex, runindex):
@@ -30,7 +32,7 @@ class RunManagementRecord:
                 self.type = "RunCommencement"
                 self.timestamp = timestamp
                 self.node = int(nodeindex)
-                self.runindex = int(runindex)
+                self.run = int(runindex)
 
         class CommunicationFailureEvent:
             def __init__(self, messagetext, timestamp, nodeindex, runindex):
@@ -38,7 +40,7 @@ class RunManagementRecord:
                 self.type = "CommunicationFailure"
                 self.timestamp = timestamp
                 self.node = int(nodeindex)
-                self.runindex = int(runindex)
+                self.run = int(runindex)
 
         class RunCompletionEvent:
             def __init__(self, messagetext, timestamp, nodeindex, runindex):
@@ -46,7 +48,15 @@ class RunManagementRecord:
                 self.type = "RunCompletion"
                 self.timestamp = timestamp
                 self.node = int(nodeindex)
-                self.runindex = int(runindex)
+                self.run = int(runindex)
+
+        class LateCompletionEvent:
+            def __init__(self, messagetext, timestamp, nodeindex, runindex=-1):
+                self.message = messagetext
+                self.type = "LateCompletion"
+                self.timestamp = timestamp
+                self.node = int(nodeindex)
+                self.run = int(runindex)
 
         class OverdueRunEvent:
             def __init__(self, messagetext, timestamp, nodeindex, runindex):
@@ -54,37 +64,71 @@ class RunManagementRecord:
                 self.type = "OverdueRun"
                 self.timestamp = timestamp
                 self.node = int(nodeindex)
-                self.runindex = int(runindex)
+                self.run = int(runindex)
+
+    class Node:
+        def __init__(self, index, workdir):
+            self.index = index
+            self.workdir = workdir
+            self.ipadress = workdir.split("\\\\")[0]
+            if "@" in workdir:
+                self.name = self.workdir.split('@')[-1].strip('".')
+                if "~" in self.name:
+                    self.hostname = self.name.split("~")[0]
+                    self.localindex = int(self.name.split("~")[1])
+                else:
+                    self.hostname = self.name
+                    self.localindex = 0
+            self.events = []
+
+        def registerevent(self, event):
+            self.events.append(event)
+
+        def getnumberofruns(self, eventtype="success"):
+            count = len([e for e in self.events if e.type == eventtype])
+            return count
+
+        def gettimesincelastevent(self):
+            then = self.events[-1].timestamp
+            now = datetime.datetime.now()
+            return now - then
 
     def __init__(self, filename):
         self.load(filename)
+        self.searchnewnodes(self.events, self.nodes)
+        self.registerevents(self.events, self.nodes)
 
     def load(self, filename):
         rmrfile = open(filename)
-
         lines = rmrfile.readlines()
-
         for l in lines:
-            self.parseLine(l)
+            self.parseline(l)
 
-    def parseLine(self, line):
+    def parseline(self, line):
 
-        def parseTime(timeString):
+        def parsetime(line):
             months = dict(Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6, Jul=7, Aug=8, Sep=9, Oct=10, Nov=11, Dec=12)
 
-            w = line.split()
-            t = w[2].split(':')
+            v = line.split()
+            t = v[2].split(':')
             s = t[2].split('.')
 
             y = int(1900)
-            m = months[w[1]]
-            d = int(w[0])
+            m = months[v[1]]
+            d = int(v[0])
             h = int(t[0])
-            min = int(t[1])
+            mn = int(t[1])
             sec = int(s[0])
             ms = int(s[1])*10000
 
-            return datetime.datetime(y, m, d, h, min, sec, ms )
+            if sec > 59:
+                sec = 0
+                mn += 1
+            if mn > 59:
+                mn = 59
+                sec = 59
+
+            return datetime.datetime(y, m, d, h, mn, sec, ms)
 
         if "PEST RUN MANAGEMENT RECORD: " in line:
             self.case = line.split()[-1]
@@ -101,35 +145,45 @@ class RunManagementRecord:
             return
 
         if "SLOW RUN DECLARATION FACTOR   :" in line:
-            self.slowRunFactor = float(line.split()[-1])
+            self.slowrunfactor = float(line.split()[-1])
             return
 
         if "assigned to node at working directory" in line:
-            timestamp = parseTime(line)
+            timestamp = parsetime(line)
             w = line.split()
-            self.events.append(self.Events.AssignmentEvent(line, timestamp, w[5], w[12]))
+            if len(w) == 13:
+                #self.events.append(self.Events.AssignmentEvent(line, timestamp, w[5], w[12]))
+                path = w[12]
+            else:
+                path = ""
+                for s in w[12:]:
+                    path += s + " "
+            self.events.append(self.Events.AssignmentEvent(line, timestamp, w[5], path.strip()))
             return
 
         if "commencing on node" in line:
-            timestamp = parseTime(line)
+            timestamp = parsetime(line)
             w = line.split()
             self.events.append(self.Events.RunCommencementEvent(line, timestamp, w[9].strip('.'), w[5]))
             return
 
         if ("communications failure on node " in line) and ("will be re-assigned to another node." in line):
-            timestamp = parseTime(line)
+            timestamp = parsetime(line)
             w = line.split()
             self.events.append(self.Events.CommunicationFailureEvent(line, timestamp, w[7].strip(';'), w[10]))
             return
 
         if " completed on node " in line:
-            timestamp = parseTime(line)
+            timestamp = parsetime(line)
             w = line.split()
-            self.events.append(self.Events.RunCompletionEvent(line, timestamp, w[9].strip('.'), w[5]))
+            if "old run so results not needed" in line:
+                self.events.append(self.Events.LateCompletionEvent(line, timestamp, w[8].strip(';')))
+            else:
+                self.events.append(self.Events.RunCompletionEvent(line, timestamp, w[9].strip('.'), w[5]))
             return
 
         if (" overdue run on node " in line) and ("may be re-assigned to another node." in line):
-            timestamp = parseTime(line)
+            timestamp = parsetime(line)
             w = line.split()
             self.events.append(self.Events.OverdueRunEvent(line, timestamp, w[7].strip(';'), w[10]))
             return
@@ -138,5 +192,42 @@ class RunManagementRecord:
         if len(line.strip()) > 0:
             self.events.append(self.Events.GeneralEvent(line))
 
+    def searchnewnodes(self, eventlist, nodelist):
+        for e in eventlist:
+            if e.type == "Assignment":
+                if e.node not in nodelist.keys():
+                    nodelist[e.node] = self.Node(e.node, e.workdir)
+
+    @staticmethod
+    def registerevents(eventlist, nodelist):
+        for e in eventlist:
+            if hasattr(e, 'node'):
+                nodelist[e.node].events.append(e)
+
 testrmr = RunManagementRecord("D:\\Repositories\\red5alex.cloudforge\\pyst\\trunk\\example_files\\runManagementRecord.rmr")
+
+
+
+outfile = open("D:\\Repositories\\red5alex.cloudforge\\pyst\\trunk\\example_files\\runManagementRecord.nodes", "w")
+#Header
+outfile.write("STR = # of started model runs\n")
+outfile.write("OK  = # of successfully finished model runs\n")
+outfile.write("DUE = # of overdue model runs\n")
+outfile.write("LAT = # of model runs finished too late\n")
+outfile.write("STR = # of communication failures\n\n")
+outfile.write("ID\tserver\tslave\tSTR\tOK\tDUE\tLAT\tCOM\tlast contact\n")
+
+for node in testrmr.nodes:
+    n = testrmr.nodes[node]
+    outfile.write(str(n.index) + "\t")
+    outfile.write(n.hostname + "\t")
+    outfile.write(str(n.localindex) + "\t\t")
+    outfile.write(str(n.getnumberofruns("RunCommencement")) + "\t")
+    outfile.write(str(n.getnumberofruns("RunCompletion")) + "\t")
+    outfile.write(str(n.getnumberofruns("OverdueRun")) + "\t")
+    outfile.write(str(n.getnumberofruns("Late")) + "\t")
+    outfile.write(str(n.getnumberofruns("CommunicationFailure")) + "\t")
+    outfile.write(str(n.gettimesincelastevent()) + "\n")
+outfile.close()
+
 pass
