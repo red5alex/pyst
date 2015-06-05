4e19794e060a83827c13d7c4e21f0fc25c53b9a0
Alexander Renz <alex.renz@gmx.net>
2015-06-05 01:04:54 +0000

    some layout updates

---
+++ TkPyst/mat2dat_gui.py
@@ -0,0 +1,42 @@
+__author__ = 'are'
+
+# command_properties.py
+import sys
+import tkinter as tk
+from tkinter import messagebox
+import pygubu
+
+pivotTable = False
+MatFilePath = ""
+# define the function callbacks
+def on_buttonOK_click():
+    messagebox.showinfo('Message', 'You clicked OK')
+        
+def on_buttonCancel_click():
+    sys.exit()
+
+class BaseApplication(pygubu.TkApplication):
+
+    def _create_ui(self):
+        #1: Create a builder
+        self.builder = builder = pygubu.Builder()
+
+        #2: Load an ui file
+        builder.add_from_file('mat2dat_gui.ui')
+
+        #3: Create the widget using self.master as parent
+        self.mainwindow = builder.get_object('Toplevel_1', self.master)
+
+        # Configure callbacks
+        callbacks = {
+            'on_buttonOK_clicked': on_buttonOK_click,
+            'on_buttonCancel_clicked': on_buttonCancel_click,
+        }
+
+        builder.connect_callbacks(callbacks)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = BaseApplication(root)
+    app.run()
