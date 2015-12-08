ec054e467bc730a022dc2ce836edecb013407d76
red5alex <red5alex@a9a4edef-5753-485f-8c01-a0eb9a0e8964>
2015-12-08 16:03:31 +0000

    now returns exit code 0

--- trunk/pysttools/ParallelPerformanceTestC.py
+++ trunk/pysttools/ParallelPerformanceTestC.py
@@ -28,38 +28,68 @@ ws.append([""])
 ws.append(["folder:", workfolder])
 ws.append(["model:", femfilename])
 ws.append([""])
-ws.append(["threads", "time"])
+
+ws.append(["threads", "time", "speedup"])
 
 os.chdir(workfolder)
 speeduptable = {}
 logfile = open(femfilename+".parperformance", "w")
+
+single_run = None
+
 for threadnumber in sequence:
     print("running model "+femfilename+" using "+str(threadnumber)+" threads:")
     t0 = time.time()
-    call(["feflow62c.exe", "-threads", str(threadnumber), femfilename])
+    time.sleep(1/threadnumber)  # dummy model for testing
+    # call(["feflow62c.exe", "-threads", str(threadnumber), femfilename])
     dt = time.time() - t0
     if dt == 0:
         dt = 0.001
     print("finished in "+str(dt))
     speeduptable[int(threadnumber)] = dt
     logfile.write("threads="+str(threadnumber)+"\t"+str(dt))
-    ws.append([threadnumber, dt.real])
+
+    if threadnumber <= 1:
+        single_run = dt
+
+    if single_run is not None:
+        speedup = single_run / dt.real
+        ws.append([threadnumber, dt.real, speedup])
+    else:
+        ws.append([threadnumber, dt.real])
 
 n = len(sequence)
 
 threadref = Reference(ws, (8, 1), (8+n, 1))
 runtimeref = Reference(ws, (8, 2), (8+n, 2))
+speedupref = Reference(ws, (8, 3), (8+n, 3))
 
-runtimeseries = Series(runtimeref, "Speedup", xvalues=threadref)
+runtimeseries = Series(runtimeref, "Run Time", xvalues=threadref)
+speedupseries = Series(speedupref, "Speedup", xvalues=speedupref)
 
 chart = ScatterChart()
 chart.append(runtimeseries)
+
+chart.append(speedupseries)
 ws.add_chart(chart)
 
-chart.title = "Speedup"
+chart.title = "Run Time"
 
 chart.x_axis.title = "Number of Threads"
 chart.y_axis.title = "Run Time [sec]"
 
 logfile.close()
-wb.save(femfilename+".parperformance.xlsx")
+outfile = femfilename+".parperformance.xlsx"
+try:
+    wb.save(outfile)
+except PermissionError:
+    while True:
+        if input("Error Saving File. Enter to retry? (enter 'n' abort)") in ["n", "N"]:
+            exit(1)
+        else:
+            try:
+                wb.save(outfile)
+                print(outfile+" written OK.")
+                break
+            except:
+                pass
